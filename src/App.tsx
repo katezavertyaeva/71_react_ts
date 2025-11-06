fbf048255a9df7b98ed1e5497f1919a70fe4bd78
katezavertyaeva <katsiarynazavertyaeva@gmail.com>
2025-11-06 21:08:04 +0100

    add lesson7 fp

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // Импорты лекций
-import Lesson06 from "./lessons/Lesson06/Lesson06";
+// import Lesson06 from "./lessons/Lesson06/Lesson06";
+import Lesson07 from "lessons/Lesson07/Lesson07";
 
 // Импорты домашних работ
 
@@ -7,7 +8,10 @@ function App() {
   return (
     <>
       {/* Лекция 6 - TypeScript */}
-      <Lesson06 />
+      {/* <Lesson06 /> */}
+
+      {/* Лекция 7 - TypeScript: object types */}
+      <Lesson07 />
     </>
   );
 }
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,9 +1,10 @@
 import "./styles.css";
+import type { InputProps } from "./types";
 
-function Input({ name, type, id, label, placeholder }) {
+function Input({ name, type = "text", id, label, placeholder }: InputProps) {
   return (
     <div className="input-container">
-      <label htmlFor={id}>{label}</label>
+      {label && <label htmlFor={id}>{label}</label>}
       <input name={name} type={type} id={id} placeholder={placeholder} />
     </div>
   );
