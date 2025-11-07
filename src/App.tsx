9918b7d39c9c484b23f44f6794ebaae378c4e0ff
katezavertyaeva <katsiarynazavertyaeva@gmail.com>
2025-11-07 21:03:41 +0100

    add lesson8 fp

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 // Импорты лекций
 // import Lesson06 from "./lessons/Lesson06/Lesson06";
-import Lesson07 from "lessons/Lesson07/Lesson07";
+// import Lesson07 from "lessons/Lesson07/Lesson07";
+import Lesson08 from "lessons/Lesson08/Lesson08";
 
 // Импорты домашних работ
+// import Homework07 from "homeworks/Homework07/Homework07";
 
 function App() {
   return (
@@ -11,7 +13,11 @@ function App() {
       {/* <Lesson06 /> */}
 
       {/* Лекция 7 - TypeScript: object types */}
-      <Lesson07 />
+      {/* <Lesson07 /> */}
+      {/* <Homework07/> */}
+
+      {/* Лекция 8 - Styling components */}
+      <Lesson08 />
     </>
   );
 }
