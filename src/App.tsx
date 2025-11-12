270b0b349c15d5e90a1abb1b29ca1701e29c1269
katezavertyaeva <katsiarynazavertyaeva@gmail.com>
2025-11-12 19:02:23 +0100

    add lesson11 fp

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import GlobalStyles from "styles/GlobalStyles";
 // import Lesson07 from "lessons/Lesson07/Lesson07";
 // import Lesson08 from "lessons/Lesson08/Lesson08";
 // import Lesson09 from "lessons/Lesson09/Lesson09";
-import Lesson10 from "lessons/Lesson10/Lesson10";
+// import Lesson10 from "lessons/Lesson10/Lesson10";
 
 // Импорты домашних работ
 // import Homework07 from "homeworks/Homework07/Homework07";
 // import Homework09 from "homeworks/Homework09/Homework09";
+import Homework10 from "homeworks/Homework10/Homework10";
 
 function App() {
   return (
@@ -30,7 +31,8 @@ function App() {
       {/* <Homework09 /> */}
 
       {/* Лекция 10 - useEffect */}
-      <Lesson10 />
+      {/* <Lesson10 /> */}
+      <Homework10 />
     </>
   );
 }
